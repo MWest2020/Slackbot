b38b8215ab1b904f1e076da104259d4398550de2
Mark West <markwesterweel@conduction.nl>
2023-09-13 10:32:45 +0200

    script done?

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
+import requests
+import pytz
 import os
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
+from datetime import datetime, timedelta
 
-# Set up the Slack client
-slack_token = os.environ["SLACK_API_TOKEN"]
-client = WebClient(token=slack_token)
+def is_dst_active():
+    local_timezone = pytz.timezone("Europe/Amsterdam")
+    now = datetime.now(local_timezone)
+    return now.dst() != timedelta(0)
 
-def post_message_to_general():
-    try:
-        # Call the chat.postMessage method using the WebClient
-        response = client.chat_postMessage(channel='#general', text=":rotating_light:ISSUES + UREN. Tot over 10 min.:rotating_light:")
-        
-        # Validate if the message was sent successfully
-        assert response["message"]["text"] == ":rotating_light:ISSUES + UREN. Tot over 10 min.:rotating_light:"
-    except SlackApiError as e:
-        # error message indicates with two lights. 
-        print(f":rotating_light::rotating_light:ISSUES + UREN. Tot over 10 min.:rotating_light:")
+def send_slack_message(token, channel, message):
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+    
+    payload = {
+        'channel': channel,
+        'text': message
+    }
+    
+    response = requests.post('https://slack.com/api/chat.postMessage', headers=headers, json=payload)
+    
+    if response.status_code != 200:
+        raise ValueError(f"Request to Slack API failed: {response.status_code}, {response.text}")
 
-# Call the function
-post_message_to_general()
+if __name__ == "__main__":
+    local_timezone = pytz.timezone("Europe/Amsterdam")
+    current_time = datetime.now(local_timezone).time()
+
+    target_time = datetime.strptime("16:30", "%H:%M").time()
+
+    if current_time.hour == target_time.hour and current_time.minute == target_time.minute:
+        SLACK_API_TOKEN = os.getenv("SLACK_API_TOKEN")  # Fetch from environment variables
+        CHANNEL_ID = '#general'  # Channel ID where the message should be posted
+        MESSAGE = ":rotating_light:ISSUES + UREN. Tot over 10 min.:rotating_light:"
+
+        send_slack_message(SLACK_API_TOKEN, CHANNEL_ID, MESSAGE)
