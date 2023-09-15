1345eb6eefd5c550da69619198c840d719d916e9
Mark West <66728126+MWest2020@users.noreply.github.com>
2023-09-15 13:55:41 +0200

    Update app.py
    testing purposes

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-import requests
-import pytz
-import os
-from datetime import datetime, timedelta
+# import requests
+# import pytz
+# import os
+# from datetime import datetime, timedelta
 
-def is_dst_active():
-    local_timezone = pytz.timezone("Europe/Amsterdam")
-    now = datetime.now(local_timezone)
-    return now.dst() != timedelta(0)
+# def is_dst_active():
+#     local_timezone = pytz.timezone("Europe/Amsterdam")
+#     now = datetime.now(local_timezone)
+#     return now.dst() != timedelta(0)
+
+# def send_slack_message(token, channel, message):
+#     headers = {
+#         'Content-Type': 'application/json',
+#         'Authorization': f'Bearer {token}'
+#     }
+    
+#     payload = {
+#         'channel': channel,
+#         'text': message
+#     }
+    
+#     response = requests.post('https://slack.com/api/chat.postMessage', headers=headers, json=payload)
+    
+#     if response.status_code != 200:
+#         raise ValueError(f"Request to Slack API failed: {response.status_code}, {response.text}")
+
+# if __name__ == "__main__":
+#     local_timezone = pytz.timezone("Europe/Amsterdam")
+#     current_time = datetime.now(local_timezone).time()
+
+#     target_time = datetime.strptime("16:30", "%H:%M").time()
+
+#     if current_time.hour == target_time.hour and current_time.minute == target_time.minute:
+#         SLACK_API_TOKEN = os.getenv("SLACK_API_TOKEN")  # Fetch from environment variables
+#         CHANNEL_ID = os.getenv("SLACK_CHANNEL_ID")    # Channel ID where the message should be posted
+#         MESSAGE = ":rotating_light:ISSUES + UREN. Tot over 10 min.:rotating_light:"
+
+#         send_slack_message(SLACK_API_TOKEN, CHANNEL_ID, MESSAGE)
+
+
+import requests
+import os
 
 def send_slack_message(token, channel, message):
     headers = {
@@ -24,15 +57,15 @@ def send_slack_message(token, channel, message):
     if response.status_code != 200:
         raise ValueError(f"Request to Slack API failed: {response.status_code}, {response.text}")
 
+    # Log the response for debugging
+    print(f"Slack API response: {response.json()}")
+
 if __name__ == "__main__":
-    local_timezone = pytz.timezone("Europe/Amsterdam")
-    current_time = datetime.now(local_timezone).time()
+    SLACK_API_TOKEN = os.getenv("SLACK_API_TOKEN")  # Fetch from environment variables
+    CHANNEL_ID = os.getenv("SLACK_CHANNEL_ID")  # Channel ID where the message should be posted
+    MESSAGE = ":rotating_light:ISSUES + UREN. Tot over 10 min.:rotating_light:"
 
-    target_time = datetime.strptime("16:30", "%H:%M").time()
-
-    if current_time.hour == target_time.hour and current_time.minute == target_time.minute:
-        SLACK_API_TOKEN = os.getenv("SLACK_API_TOKEN")  # Fetch from environment variables
-        CHANNEL_ID = os.getenv("SLACK_CHANNEL_ID")    # Channel ID where the message should be posted
-        MESSAGE = ":rotating_light:ISSUES + UREN. Tot over 10 min.:rotating_light:"
-
-        send_slack_message(SLACK_API_TOKEN, CHANNEL_ID, MESSAGE)
+    # Print the variables for debugging (excluding the token for security)
+    print(f"Channel ID: {CHANNEL_ID}")
+    
+    send_slack_message(SLACK_API_TOKEN, CHANNEL_ID, MESSAGE)
